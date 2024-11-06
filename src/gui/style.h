f63572f81e2d4fb4bcfa5849ef56af3b76e4d4bc
kaan <kaandesu00@gmail.com>
2024-11-06 09:59:36 +0100

    feat: common style header definitions

---
+++ src/gui/style.h
@@ -0,0 +1,20 @@
+#ifndef STYLE_H
+#define STYLE_H
+
+/* sizes */
+#define XXS 8
+#define XS 20
+#define SM 24
+#define MD 36
+#define LG 48
+#define XL 72
+
+/* paddings */
+#define P_XXS XXS
+#define P_XS XS
+#define P_SM SM
+#define P_MD MD
+#define P_LG LG
+#define P_XL XL
+
+#endif
